a7b8e58e6839fb34f539e4c0caf1379207332638
Charith <charithhewage@gmail.com>
2018-07-19 13:13:58 +1000

    address form field not showling issue fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_052805) do
+ActiveRecord::Schema.define(version: 2018_07_19_030217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2018_07_18_052805) do
     t.string "zip_code", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "house_id"
+    t.index ["house_id"], name: "index_addresses_on_house_id"
   end
 
   create_table "checkboxes", force: :cascade do |t|
@@ -76,4 +78,5 @@ ActiveRecord::Schema.define(version: 2018_07_18_052805) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "addresses", "houses"
 end
